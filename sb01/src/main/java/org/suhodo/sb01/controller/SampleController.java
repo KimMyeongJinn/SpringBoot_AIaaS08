868f1e9a37f66f193afdc520e0cb4d3fee09fa9e
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-22 11:34:08 +0900

    @Controller와 @RestController의 차이

---
+++ sb01/src/main/java/org/suhodo/sb01/controller/SampleController.java
@@ -0,0 +1,33 @@
+package org.suhodo.sb01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/* Facade 패턴
+FrontController - PageController의 이중 구조
+FrontController : tomcat에서 데이터를 우선 처리/공통 처리하는 부분
+                  주소와 매핑된 PageController로 전달한다.
+                  스프링에서는 DispatcherServlet이 담당한다.
+PageController :  FrontController에서 넘겨받은 데이터를 처리
+                  이곳에서 jsp/thymeleaf로 데이터를 전달
+jsp/thymeleaf :  PageController에서 넘겨받은 데이터를
+                html코드 부분에 배치시켜서
+                브라우저에 렌더링 될 화면
+                내부적으로는 java로 변환된다.
+* */
+
+// PageController라는 어노테이션
+@Controller
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/hello")
+    public void hello(Model model){
+        log.info("hello....................");
+
+        // templates/hello.html로 아래 데이터 전달
+        model.addAttribute("msg", "HELLO WORLD");
+    }
+}
