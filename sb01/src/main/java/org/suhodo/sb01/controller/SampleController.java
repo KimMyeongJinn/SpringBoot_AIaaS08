066bb191f004532b430666ab1e01268f601a6332
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-22 15:50:05 +0900

    thymeleaf의 사용법 완료

--- sb01/src/main/java/org/suhodo/sb01/controller/SampleController.java
+++ sb01/src/main/java/org/suhodo/sb01/controller/SampleController.java
@@ -6,7 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /* Facade 패턴
 FrontController - PageController의 이중 구조
@@ -60,6 +64,32 @@ public class SampleController {
     @GetMapping("/ex/ex2")
     public void ex2(Model model){
         log.info("ex/ex2....................");
+
+        List<String> strList = IntStream.range(1, 10)
+                .mapToObj(i -> "Data" + i)
+                .collect(Collectors.toList());
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+        model.addAttribute("map", map);
+
+        SampleDTO dto = new SampleDTO();
+        dto.p1 = "Value -- p1";
+        dto.p2 = "Value -- p2";
+        dto.p3 = "Value -- p3";
+        model.addAttribute("dto", dto);
+    }
+
+    @GetMapping("/ex/ex3")
+    public void ex3(Model model){
+        model.addAttribute("arr", new String[]{"AAA", "BBB", "CCC"});
+    }
+    
+    @GetMapping("/ex/ex4")
+    public void ex4(Model model){
+        model.addAttribute("iu", "아이유 사진");
     }
 }
 
