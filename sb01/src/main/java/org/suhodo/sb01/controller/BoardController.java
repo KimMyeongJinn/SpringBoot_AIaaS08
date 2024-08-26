ba3cc71602947917d2fb560642b3b3b89b8caf20
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-26 17:28:59 +0900

    /board/register 처리와 register.html 작성

--- sb01/src/main/java/org/suhodo/sb01/controller/BoardController.java
+++ sb01/src/main/java/org/suhodo/sb01/controller/BoardController.java
@@ -4,12 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suhodo.sb01.dto.BoardDTO;
 import org.suhodo.sb01.dto.PageRequestDTO;
 import org.suhodo.sb01.dto.PageResponseDTO;
 import org.suhodo.sb01.service.BoardService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -28,4 +33,45 @@ public class BoardController {
 
         model.addAttribute("responseDTO", responseDTO);
     }
+
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+        log.info("board POST register.....");
+
+        // @Valid처리를 통해 BoardDTO의 제약사항에 위배되면
+        // 아래 에러가 발생한다.
+        if(bindingResult.hasErrors()){
+            log.info("has errors..........");
+            // redirect전송 시 처음 1번 "errors"값을 꺼내도록 전송한다.
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+            return "redirect:/board/register";
+        }
+
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
