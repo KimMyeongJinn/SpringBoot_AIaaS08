55715de12424d8187ec7f870c1bfc41c3f7e8567
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-29 11:43:32 +0900

    파일 업로드를 위한 UploadFileDTO와 UpDownController 생성

---
+++ sb01/src/main/java/org/suhodo/sb01/controller/UpDownController.java
@@ -0,0 +1,21 @@
+package org.suhodo.sb01.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.suhodo.sb01.dto.UploadFileDTO;
+
+@RestController
+@Log4j2
+public class UpDownController {
+    
+    @ApiOperation(value = "Upload POST", notes = "POST 방식으로 파일 등록")
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String upload(UploadFileDTO uploadFileDTO){
+        log.info(uploadFileDTO);
+
+        return null;
+    }
+}
