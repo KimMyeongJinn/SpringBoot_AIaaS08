e4cf5f52b79f9dc1f0f20d685b413cce34389996
bitcamp\aiaas <navercloudplatform08@gmail.com>
2024-08-29 12:31:30 +0900

    public class UpDownController {
        @Value("${org.suhodo.upload.path}")
        private String uploadPath;
    }
    
    @Value를 lombok.Value로 하지 않고,
    org.springframework.beans.factory.annotation의 Value를
    사용해야 한다.
    
    안 그러면 QueryDsl의 QBoard를 찾지 못하는 현상이 벌어진다.

--- sb01/src/main/java/org/suhodo/sb01/controller/UpDownController.java
+++ sb01/src/main/java/org/suhodo/sb01/controller/UpDownController.java
@@ -1,8 +1,8 @@
 package org.suhodo.sb01.controller;
 
 import io.swagger.annotations.ApiOperation;
-import lombok.Value;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
